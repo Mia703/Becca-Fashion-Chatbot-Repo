3593f80f5a3d1ad44fd4a8a7d66b5d1869f90f84
Mia703 <bookworm7572@gmail.com>
2023-03-15 14:50:53 -0400

    Updating Base Files
    - Updated Readme
    - Added Becca.py
    - Added Macros.py
    - Added Ontology.json

---
+++ Becca.py
@@ -0,0 +1,28 @@
+from emora_stdm import DialogueFlow
+from typing import Dict, Any
+
+def matching_strategies() -> Dict[str, Any]:
+    return {
+        'state': 'start',
+        '`Hello. How are you?`': {
+            '[{good, fantastic}]': {
+                '`Glad to hear that you are doing well :)`': 'end'
+            },
+            '[{bad, could be better}]': {
+                '`I hope your day gets better soon :(`': 'end'
+            },
+            '[{how, and}, {you, going}]': {
+                '`I feel superb. Thank you!`': 'end'
+            },
+            'error': {
+                '`Sorry, I didn\'t understand you.`': 'end'
+            },
+        }
+    }
+
+
+df = DialogueFlow('start', end_state='end')
+df.load_transitions(matching_strategies())
+
+if __name__ == '__main__':
+    df.run()
