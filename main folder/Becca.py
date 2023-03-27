6f23b7509f09b90db7ff4db6d848f1e725f5003b
Mia703 <bookworm7572@gmail.com>
2023-03-27 10:59:10 -0400

    Events Macro
    - So thought about putting it into a Macro but how do I pass user input as a parameter? And then if I'm looking for an 'event' keyword, I'd have to use regEx. Which is ew...

--- main folder/Becca.py
+++ main folder/Becca.py
@@ -1,24 +1,11 @@
-import csv
-import re
+import pandas as pd
 from typing import Dict, Any, List
 from emora_stdm import Macro, Ngrams, DialogueFlow
 
 
 # variables ======================================================
 
-# the word I'm looking for
-keyword = ""
-
 # macros ======================================================
-
-# searches for the keyword in the csv file
-class MacroSearchKeyword(Macro):
-	def run(self, ngrams: Ngrams, vars: Dict[str, Any], args: List[str]):
-		global keyword
-
-		print(ngrams.raw_text())
-		return
-
 
 # the dialogue ======================================================
 def main_dialogue() -> DialogueFlow:
@@ -43,14 +30,31 @@ def main_dialogue() -> DialogueFlow:
 		'state': 'dresscode_transition',
 		'`\n \nI know that a lot of young people have a hard time understanding the rules of a dress-code specific event. \n'
 		'I\'ll help you decipher it! Do you have any upcoming dress-code specific events?`': {
-			# '#RETURN_DRESS_CODE': {
-			# 	'`Got it.`': 'end'
-			# },
-			# '{<"black tie">, <"white tie">, <"cocktail">, <"business formal">, <"business casual">}': {
-			# 	'`Got it` #RETURN_DRESS_CODE': 'end'
-			# },
-			'{<"black tie">, <"black-tie">}': {
-				'`Got it. You have a black tie event right?`': 'end'
+			'<"black tie">': {
+				'`Got it. You have a black tie event right? '
+				'\n Black tie events are usually a formal event that usually requires men to wear a tuxedo '
+				'and women to wear an evening gown.`': 'end'
+			},
+			'<"white tie">': {
+				'`Got it. You have a white tie event right? '
+				'\n White tie events are the most formal dress code and usually requires men to wear a black tailcoat,'
+				' a white bow tie, and black trousers, and women to wear a floor-length formal gown.`': 'end'
+			},
+			'<"cocktail">': {
+				'`Got it. You have a cocktail event right? '
+				'\n Cocktail events are a semi-formal event where men typically wear a suit and tie '
+				'and women wear a cocktail dress or a dressy skirt and blouse.`': 'end'
+			},
+			'<"business formal">': {
+				'`Got it. You have a business formal event right? '
+				'\n Business formal events are a dress code for professional settings, such as a job interview, '
+				'where men typically wear a suit and tie and women wear a suit, dress or blouse and skirt.`': 'end'
+			},
+			'<"business casual">': {
+				'`Got it. You have a business casual event right?` '
+				'\n Business casual events are less formal than business formal but still requires professional attire, '
+				'such as slacks and a button-down shirt for men and a skirt or '
+				'dress pants with a blouse or sweater for women.': 'end'
 			},
 			'error': {
 				'`Sorry, I don\'t understand`': 'end'
@@ -60,7 +64,7 @@ def main_dialogue() -> DialogueFlow:
 
 	# macro references ======================================================
 	macros = {
-		'RETURN_DRESS_CODE': MacroSearchKeyword(),
+
 	}
 
 	# ======================================================
